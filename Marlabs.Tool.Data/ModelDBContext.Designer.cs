53839bbd563068b1e9327b184d8cc824b57a1470
Kiran.Naveena <Kiran.Naveena@MLBMYSL-12306>
2019-06-08 00:35:26 +0530

    Entity related changes

--- Marlabs.Tool.Data/ModelDBContext.Designer.cs
+++ Marlabs.Tool.Data/ModelDBContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\mHack HokerUnion\honkerunion\Marlabs.Tool.Data\ModelDBContext.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\TestRepo\Marlabs.Tool.Data\ModelDBContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
